53ae6016203857fb4d84a54e1a9b1eb76408f18b
Yusuke <now.good.1206@gmail.com>
2020-07-12 21:05:58 +0900

    試作品

---
+++ todo_app/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  resources :tasks #taskscontrollerに7つのアクションを設定できる
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
